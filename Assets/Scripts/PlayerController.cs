f6c7fce450b4588fdfe60ca968b2f97a1fa4027c
nathan241 <98729291+nathan241@users.noreply.github.com>
2022-03-20 12:37:51 -0700

    JTran_Unit3
    using force properties to enable the player to jump

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody rbPlayer;
+    public float gravityModifier;
+    public float jumpForce;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rbPlayer = GetComponent<Rigidbody>();
+        Physics.gravity *= gravityModifier;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool spaceDown = Input.GetKeyDown(KeyCode.Space);
+        if (spaceDown)
+        {
+            rbPlayer.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+    }
+}
